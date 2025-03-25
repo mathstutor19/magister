756741499ec56d5bf63e86c7a178fbc9ec00f410
mathstutor19 <mathstutor19@gmail.com>
2025-03-25 09:28:38 +0500

    change images url

--- src/data.js
+++ src/data.js
@@ -3,18 +3,18 @@ export const data = [
         id: 1,
         name: "Suvchechak",
         detail: "Badanda kichik qizil pufakcha paydo bo‘lishi, Bosh og’rig’i, Ishtaha yo’qolishi, va isitma chiqishi",
-        image: "/public/assets/suvchechak.jpg",
+        image: "https://mymedic.uz/wp-content/uploads/2017/09/vvo-vetryanka-960_640.jpg",
     },
     {
         id: 2,
         name: "Vitiligo (Pes)",
         detail: "Oq dog‘lar paydo bo‘lishi , Dog‘larning asta-sekin kengayishi, Sochlarning oqarishi, va Terining sezgirligi o‘zgarishi",
-        image: "/public/assets/pes.jpg",
+        image: "https://mymedic.uz/wp-content/uploads/vitiligo.jpg",
     },
     {
         id: 3,
         name: "Allergiya",
         detail: "Burun bitishi, Ko’zlarning qichishi, Burundagi qichishish, Ko’zlarning yoshlanishi, Yo’talish, va isitma chiqishi",
-        image: "/public/assets/allergiya.jpg",
+        image: "https://mymedic.uz/wp-content/uploads/2017/09/woman-allergy_960_6401.jpg",
     },
 ];
